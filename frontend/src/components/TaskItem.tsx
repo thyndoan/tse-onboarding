0799064979cede8b30cd9375950a4ae1919dccd2
Thy Doan <tndoan@ucsd.edu>
2026-05-08 14:36:08 -0700

    Revert "task 1.2"

--- frontend/src/components/TaskItem.tsx
+++ frontend/src/components/TaskItem.tsx
@@ -1,6 +1,4 @@
-import { Dialog } from "@tritonse/tse-constellation";
-import React, { useState } from "react";
-import { updateTask } from "src/api/tasks";
+import React from "react";
 import { CheckButton } from "src/components";
 import styles from "src/components/TaskItem.module.css";
 
@@ -10,31 +8,10 @@ export type TaskItemProps = {
   task: Task;
 };
 
-export function TaskItem({ task: initialTask }: TaskItemProps) {
-  const [task, setTask] = useState<Task>(initialTask);
-  const [isLoading, setLoading] = useState<boolean>(false);
-  const [errorModalMessage, setErrorModalMessage] = useState<string | null>(null);
-
-  const handleToggleCheck = async () => {
-    setLoading(true);
-    const result = await updateTask({ ...task, isChecked: !task.isChecked });
-    if (result.success) {
-      setTask(result.data);
-    } else {
-      setErrorModalMessage(result.error);
-    }
-    setLoading(false);
-  };
-
+export function TaskItem({ task }: TaskItemProps) {
   return (
     <div className={styles.item}>
-      <CheckButton
-        checked={task.isChecked}
-        onPress={() => {
-          void handleToggleCheck();
-        }}
-        disabled={isLoading}
-      />
+      <CheckButton checked={task.isChecked} />
       <div
         className={
           task.isChecked ? `${styles.textContainer} ${styles.checked}` : styles.textContainer
@@ -43,14 +20,6 @@ export function TaskItem({ task: initialTask }: TaskItemProps) {
         <span className={styles.title}>{task.title}</span>
         {task.description && <span className={styles.description}>{task.description}</span>}
       </div>
-      <Dialog
-        styleVersion="styled"
-        variant="error"
-        title="An error occurred"
-        content={<p>{errorModalMessage}</p>}
-        isOpen={errorModalMessage !== null}
-        onClose={() => setErrorModalMessage(null)}
-      />
     </div>
   );
 }
